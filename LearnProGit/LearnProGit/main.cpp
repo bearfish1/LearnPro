f5e1b1db50ad32151ed5ac407df00742a15b40e1
xiongkang <xiongk_hust@163.com>
2021-04-03 17:32:57 +0800

    First Commit

---
+++ LearnProGit/LearnProGit/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearnProGit
+//
+//  Created by 熊康 on 2021/4/3.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
